a4d37d4cc45cf96bbed01511df6a9a764b8109d3
nikomm13 <149721319+nikomm13@users.noreply.github.com>
2023-11-23 01:28:40 -0500

    Medicamento.java
    Signed-off-by: nikomm13 <149721319+nikomm13@users.noreply.github.com>

--- src/Medicamento.java
+++ src/Medicamento.java
@@ -1,8 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Medicamento {
-    public String nombre;
-    public int costo;
+    Scanner sc = new Scanner(System.in);
+    protected String nombre;
+    protected int costo;
+    List<Medicamento> lstMedicamentos = new ArrayList<>();
     public Medicamento(String nombre, int costo) {
         this.nombre = nombre;
         this.costo = costo;
+    }
+    public Medicamento(){
+
+    }
+    public Medicamento getMedicamento(int pos){
+        return lstMedicamentos.get(pos);
+    }
+
+    public void setMedicamento(Medicamento objM){
+        lstMedicamentos.add(objM);
+    }
+
+    public int listaMedicamentos(){
+        for (int i = 0; i < lstMedicamentos.size(); i ++){
+            System.out.println(i + " " + lstMedicamentos.get(i).getNombre());
+        }
+        int op;
+        op =   Integer.parseInt(sc.nextLine());
+        if (op < 0 || op>= lstMedicamentos.size()){
+            return listaMedicamentos();
+        }
+        return op;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public String toString(){
+        return "Medicamento: "+nombre+costo;
     }
 }
