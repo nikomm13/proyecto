3ad1f16e2519d57922aa8b860cdd91702584849f
nikomm13 <149721319+nikomm13@users.noreply.github.com>
2023-11-23 01:27:31 -0500

    Historial.java
    Signed-off-by: nikomm13 <149721319+nikomm13@users.noreply.github.com>

--- src/Historial.java
+++ src/Historial.java
@@ -1,3 +1,21 @@
-public class Historial {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Historial {
+    public Consulta consulta;
+    List<Consulta> lstConsulta;
+
+    public void setConsulta(Consulta consulta){
+        this.consulta = consulta;
+    }
+    public Historial() {
+        lstConsulta = new ArrayList<Consulta>();
+    }
+    public List<Consulta> getConsultas() {
+        return lstConsulta;
+    }
+    public void agregarConsulta(Consulta consulta) {
+        lstConsulta.add(consulta);
+    }
 }
+
