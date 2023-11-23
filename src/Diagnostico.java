3de1c9a690e9ed00e41680f0fe58e96a3e907d6f
nikomm13 <149721319+nikomm13@users.noreply.github.com>
2023-11-23 01:25:26 -0500

    Diagnostico.java
    Signed-off-by: nikomm13 <149721319+nikomm13@users.noreply.github.com>

--- src/Diagnostico.java
+++ src/Diagnostico.java
@@ -1,4 +1,40 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Diagnostico {
-    public String diagnostico;
+    private String diagnostico;
+    private Examen examen;
+    private LocalDate fecha;
+
+    List<Examen> lstExamenes = new ArrayList<>();
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public Examen getExamen() {
+        return examen;
+    }
+
+    public void setExamen(Examen examen) {
+        this.examen = examen;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+    public String toString(){
+        return "este fue el diagnostico: "+diagnostico+" se remiten los siguientes examenes: "+examen+fecha;
+    }
 
 }
+
