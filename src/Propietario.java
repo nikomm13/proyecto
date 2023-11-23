865f258587268b01705128d6ee4d525482a0979a
nikomm13 <149721319+nikomm13@users.noreply.github.com>
2023-11-23 01:29:03 -0500

    Propietario.java
    Signed-off-by: nikomm13 <149721319+nikomm13@users.noreply.github.com>

--- src/Propietario.java
+++ src/Propietario.java
@@ -1,15 +1,70 @@
-public class Propietario {
-    public long id;
-    public String nombre;
-    public String mail;
-    public String direccion;
-    public long telefono;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Propietario {
+    private long id;
+    private String nombre;
+    private String mail;
+    private String direccion;
+    private long telefono;
+
+    public Propietario(){
+
+    }
+    List<Propietario> lstPropietarios = new ArrayList<>();
     public Propietario(long id, String nombre, String direccion, String mail, long telefono){
         this.id = id;
         this.nombre = nombre;
         this.direccion = direccion;
         this.mail = mail;
+        this.telefono = telefono;
+    }
+
+    public Propietario getMascota(int pos){
+        return lstPropietarios.get(pos);
+    }
+
+    public void setMascota(Propietario objP){
+        lstPropietarios.add(objP);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
         this.telefono = telefono;
     }
 }
